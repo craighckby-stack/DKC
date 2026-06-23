d68c32dfa6eaecd6dbbcd3bb5e67eacc6c26003a
Craig Huckerby <craighckby@gmail.com>
2026-06-23 23:55:22 +1000

    [DARLEK CANN] Auto-create supplementary file: vite-env.d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,10 +1,14 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly VITE_PORT: string;
   readonly DISABLE_HMR: string;
-  readonly VITE_API_URL: string;
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+declare const __BUILD_TIMESTAMP__: string;
+declare const __AGENT_CORE_VERSION__: string;
+declare const __ENV_MODE__: string;
